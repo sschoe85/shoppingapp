209f945b96e0f1a34efe86bb0f1e4b1701763e6f
sschoe85 <s@bine-schoetensack.eu>
2020-12-08 14:30:37 +0100

    Add SASS

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function HomePage(props) {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'></div>
+                    <h1 className='title'>HATS</h1>
+                    <span className= 'subtitle'></span>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
