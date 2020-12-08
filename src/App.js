7785e004b98cc65eadcacd1ae71f2b19dfd21b7e
sschoe85 <s@bine-schoetensack.eu>
2020-12-08 19:17:24 +0100

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 
 
 function App() {
   return (
+    <>
+    <Header/>
     <Switch>
       <Route exact path = '/' component ={HomePage}/>
       <Route exact path = '/shop' component= {ShopPage}/>
     </Switch>
+    </>
   );
 }
 
