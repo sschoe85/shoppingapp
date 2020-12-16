df7472cf1b85475e25e91b3db4b42272bbf185d4
sschoe85 <s@bine-schoetensack.eu>
2020-12-16 12:25:54 +0100

    Group same items in the shopping cart, update data with unique ids

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd
+  );
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+//pay attention to create new Objects and Arrays so that state can be updated instead of modifying existing arrays!
