5b31a940c8559f4201865c10945cc867326d35f8
sschoe85 <s@bine-schoetensack.eu>
2020-12-20 18:18:30 +0100

    Implement remove singular intem from cart functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types.js";
-import { addItemToCart } from "./cart.utils.js";
+import { addItemToCart, removeItemFromCart } from "./cart.utils.js";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -25,6 +25,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
           cartItems: state.cartItems.filter(item => item.id !== action.payload.id)
         }
 
+        case CartActionTypes.REMOVE_ITEM:
+          return{
+            ...state,
+            cartItems: removeItemFromCart(state.cartItems, action.payload)
+          }
     default:
       return state;
   }
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,4 +12,11 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
 
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToRemove.id && cartItem.quantity > 1
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+    );
+}
 //pay attention to create new Objects and Arrays so that state can be updated instead of modifying existing arrays!
