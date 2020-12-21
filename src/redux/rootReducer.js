610e7ef64c8f10f74a58daa56cdf0e163f95ed92
sschoe85 <s@bine-schoetensack.eu>
2020-12-21 11:38:29 +0100

    Move directory state into redux

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,6 +3,7 @@ import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
 import storage from 'redux-persist/lib/storage' //local storage
 import persistReducer from "redux-persist/es/persistReducer";
+import directoryReducer from "./directory/directory.reducer";
 
 
 const persistConfig = {
@@ -14,6 +15,7 @@ const persistConfig = {
 const rootReducer =  combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
